86ffbc91ffd74168a1071761fe18283052046e3c
huangshuwei <hsw89520@163.com>
2016-11-02 18:02:41 +0800

    vue demos

---
+++ 15-单文件组件/hello.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>{{greeting}} World!</p>
+</template>
+
+<script>
+
+    module.exports = {
+        data:function () {
+            return{
+                greeting:'Hello'
+            }
+        }
+    }
+
+</script>
+
+
+<style scoped>
+    p{
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
